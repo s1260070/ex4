5d2f3768d6a6daffbfa13f92b3cb85b015e58b5d
s1260070 <s1260070@u-aizu.ac.jp>
2019-07-06 20:27:17 +0900

    Initial revision

---
+++ DiceGame.java
@@ -0,0 +1,16 @@
+
+import java.util.Random;
+
+public class DiceGame{
+    static public void main(String[] args){
+	Random rnd = new Random();
+	int dice1 = rnd.nextInt(6)+1;
+	int dice2 = rnd.nextInt(6)+1;
+	int sum = dice1+dice2;
+	System.out.println("Rolling the dice...");
+	System.out.println("Die 1: "+dice1);
+	System.out.println("Die 2: "+dice2);
+	System.out.println("Total value: "+sum);
+
+    }
+}
