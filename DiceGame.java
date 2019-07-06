7ab90d1dfbe82afd2506a913a84040536a770760
s1260070 <s1260070@u-aizu.ac.jp>
2019-07-06 20:28:38 +0900

    Greetings added

--- DiceGame.java
+++ DiceGame.java
@@ -1,8 +1,20 @@
 
+import java.io.*;
 import java.util.Random;
 
 public class DiceGame{
     static public void main(String[] args){
+	String name = "";
+	System.out.println("What is your name?");
+	InputStreamReader isr = new InputStreamReader(System.in);
+	BufferedReader br = new BufferedReader(isr);
+	try{
+	    name = br.readLine();
+	}
+	catch(Exception e){
+	}
+	System.out.println("Hello, "+name+"!");
+
 	Random rnd = new Random();
 	int dice1 = rnd.nextInt(6)+1;
 	int dice2 = rnd.nextInt(6)+1;
