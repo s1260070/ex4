7d27f8f22df4104c3ffc606c22c82d1736c54371
s1260070 <s1260070@u-aizu.ac.jp>
2019-07-06 20:31:54 +0900

    My feature added

--- DiceGame.java
+++ DiceGame.java
@@ -23,8 +23,29 @@ public class DiceGame{
 	System.out.println("Die 1: "+dice1);
 	System.out.println("Die 2: "+dice2);
 	System.out.println("Total value: "+sum);
-	if(sum>=7)System.out.println("You won");
-	else System.out.println("You lost");
+  if(sum>=7)System.out.println(name+" won");
+  else System.out.println(name+" lost");
 
-    }
+  //my original feature
+  System.out.println("Do you want to try again? -> Y/n");
+  String answer = "";
+	InputStreamReader isr1 = new InputStreamReader(System.in);
+	BufferedReader br1 = new BufferedReader(isr);
+	try{
+	    answer = br1.readLine();
+	}
+	catch(Exception e){
+	}
+  if(answer.equals("Y")){
+    int dice11 = rnd.nextInt(6)+1;
+    int dice21 = rnd.nextInt(6)+1;
+    int sum1 = dice11+dice21;
+    System.out.println("Rolling the dice...");
+    System.out.println("Die 1: "+dice11);
+    System.out.println("Die 2: "+dice21);
+    System.out.println("Total value: "+sum);
+    if(sum1>=7)System.out.println(name+" won");
+    else System.out.println(name+" lost");
+  }
+}
 }
